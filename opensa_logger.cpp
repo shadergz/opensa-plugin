8587ee24e21d0fa48968840635d3d1a1077b50f9
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-10-07 14:35:00 -0400

    [v0.0.0] - A Log client service has been implemented

---
+++ opensa_logger.cpp
@@ -0,0 +1,105 @@
+#include <cstdio>
+#include <cstring>
+
+#include <opensa_logger.h>
+
+constexpr const char* const LOGD_TAG = "OpenSA";
+
+static Client_Log::LOG_Options m_default_log;
+
+namespace Client_Log {
+    OpenSA_Logger::OpenSA_Logger() noexcept {
+        m_default_log.m_TAG = LOGD_TAG;
+        /* Copy and initialize the default logger options structure data */
+        /* m_log_options = std::make_unique<LOG_Options>(m_default_log); */
+        m_log_options = m_default_log;
+    }
+
+    void OpenSA_Logger::Android_Release(const LOG_Release_Info* release_info) {
+        if (m_log_options.m_use_logcat)
+        {
+            __android_log_write(release_info->m_prior, release_info->m_TAG_name, 
+                release_info->m_output_buffer);
+        }
+    }
+
+    ssize_t OpenSA_Logger::Android_Produce(LOG_Release_Info* produce_info) {
+        switch(produce_info->m_prior)
+        {
+        default:
+        case ANDROID_LOG_UNKNOWN:
+        case ANDROID_LOG_DEFAULT:
+        case ANDROID_LOG_VERBOSE:
+        case ANDROID_LOG_DEBUG:
+        case ANDROID_LOG_WARN:
+        case ANDROID_LOG_ERROR:
+        case ANDROID_LOG_FATAL:
+        case ANDROID_LOG_SILENT:
+        case ANDROID_LOG_INFO: produce_info->m_status_str = "Info"; break;
+        }
+
+        uintptr_t buffer_ptr_location = 0, remain_buffer_sz;
+        char* const base_buffer_ptr = produce_info->m_output_buffer;
+
+        const LOG_Location* location = &produce_info->log_local;
+
+        #define OUTPUT_LOCATION\
+            (base_buffer_ptr + buffer_ptr_location)
+        
+        #define REMAIN_SIZE\
+            (FORMAT_OUTPUT_SZ - buffer_ptr_location)
+        
+        #define INC_BUFFER(size)\
+            (buffer_ptr_location += size)
+        
+        #define EXPAND_BUFFER(message, ...)\
+            INC_BUFFER(snprintf(OUTPUT_LOCATION, REMAIN_SIZE, message, ##__VA_ARGS__))
+
+        /* (TAG FILE:LINE) <Status> -> MESSAGE */
+
+        if (m_log_options.m_dsp_TAG)
+        {
+            EXPAND_BUFFER("(%s ", m_log_options.m_TAG);
+        }
+        if (m_log_options.m_file_status)
+        {
+            EXPAND_BUFFER("%s:%d) ", location->m_filename, location->m_line); 
+        } else
+        { 
+            EXPAND_BUFFER(") "); 
+        }
+
+        if (m_log_options.m_dsp_status)
+        {
+            EXPAND_BUFFER("<%s> ", produce_info->m_status_str);
+        }
+
+        EXPAND_BUFFER("-> %s", produce_info->m_format_buffer);
+
+        return static_cast<ssize_t>(buffer_ptr_location);
+    }
+
+    ssize_t OpenSA_Logger::Android_Launch(const LOG_Launch_Data* launch_data, const char* format, ...) {
+        va_list variable_arguments;
+
+        va_start(variable_arguments, format);
+
+        LOG_Release_Info stack_based_re = {
+            .m_TAG_name = m_log_options.m_TAG,
+            .m_prior = launch_data->m_priority
+        };
+
+        memcpy(&stack_based_re.log_local, launch_data->m_location, sizeof(LOG_Location));
+        vsnprintf(stack_based_re.m_format_buffer, FORMAT_BUFFER_SZ, format, variable_arguments);
+
+        const auto produce_result = Android_Produce(&stack_based_re);
+
+        Android_Release(&stack_based_re);
+
+        va_end(variable_arguments);
+
+        return produce_result;
+    }
+
+};
+
---
+++ opensa_logger.h
@@ -0,0 +1,96 @@
+#ifndef OPENSA_LOGGER_H
+#define OPENSA_LOGGER_H
+
+#include <android/log.h>
+
+/* #include <memory> */
+#include <cstdarg>
+#include <cstdint>
+
+namespace Client_Log {
+    constexpr short FORMAT_BUFFER_SZ = 0x4f;
+    constexpr short FORMAT_OUTPUT_SZ = FORMAT_BUFFER_SZ * 2;
+
+    struct LOG_Location
+    {
+    public:
+        const char* m_filename;
+        uint32_t m_line;
+    };
+
+    struct LOG_Release_Info {
+    public:
+        const char* m_TAG_name;
+        const char* m_status_str;
+
+        const android_LogPriority m_prior;
+        LOG_Location log_local;
+
+        char m_format_buffer[FORMAT_BUFFER_SZ];
+        char m_output_buffer[FORMAT_OUTPUT_SZ];
+    };
+
+    struct LOG_Launch_Data {
+    public:
+        const android_LogPriority m_priority;
+        const LOG_Location* m_location;
+    };
+
+    struct LOG_Options {
+    public:
+        LOG_Options() {}
+        LOG_Options(const LOG_Options& copy_options) {
+            memcpy(this, &copy_options, sizeof(copy_options));
+        }
+        bool m_dsp_TAG = true;
+        bool m_dsp_status = true;
+        /* Display information like the file and line where the log originate from */
+        bool m_file_status = true;
+
+        const char* m_TAG = nullptr;
+        /* -> When enable by default, the log system will display the message into the 
+         * Android logcat message system, otherwise, only the file descriptor will be used into
+         * log release operations <-
+        */
+        bool m_use_logcat = true;
+        int m_opened_FD = -1;
+    };
+
+    class OpenSA_Logger {
+    public:
+        /*
+        OpenSA_Logger(LOG_Options* log_options);
+        OpenSA_Logger(int opened_fd, const char* log_filename = nullptr);
+        */
+        OpenSA_Logger() noexcept;
+        ~OpenSA_Logger() = default;
+
+        #define Android_Info(logger, format, ...)\
+            do \
+            {\
+                Client_Log::LOG_Location __actual_location {\
+                    .m_filename = __FILE__,\
+                    .m_line = __LINE__\
+                };\
+                const Client_Log::LOG_Launch_Data __current_log_launch {\
+                    .m_priority = ANDROID_LOG_INFO,\
+                    .m_location = &__actual_location\
+                };\
+                logger.Android_Launch(&__current_log_launch, format, __VA_ARGS__);\
+            }\
+            while(0)
+
+        ssize_t Android_Launch(const LOG_Launch_Data* launch_data, const char* format, ...);
+        void configure_user_output(int opened_fd, const char* opt_filename = nullptr);
+
+    private:
+        ssize_t Android_Produce(LOG_Release_Info* produce_info);
+        void Android_Release(const LOG_Release_Info* release_info);
+        /* std::unique_ptr<LOG_Options> m_log_options; */
+        LOG_Options m_log_options;
+    };
+
+};
+
+#endif
+

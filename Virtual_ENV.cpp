405748bcee0fb9c6b3d8e8be7a7ae7c7a207cc17
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-10-13 11:48:58 -0400

    [v0.0.1] - Virtual ENV objects handler added

---
+++ Virtual_ENV.cpp
@@ -0,0 +1,44 @@
+#include <opensa_logger.h>
+#include <opensa_objects.h>
+
+static int gMessageStatus = 0;
+
+namespace OpenSA {
+
+    void JVM_Objects::Init_Load_Objects(int* env_Status, jobject javaContext) {
+        Android_Info(gMAIN_SA_Logger, gMessageStatus, "Loading some JNI methods\n");
+        /* Loading Toast class and methods */
+        if (javaContext == nullptr)
+        {
+            Android_Error(gMAIN_SA_Logger, gMessageStatus, "The context object is nullptr, this is an cruel error\n");
+            return;
+        }
+        mThizContext = javaContext;
+
+        mToast_Class = gMAIN_Env->FindClass("android/widget/Toast");
+        mToast_MakeTextID = gMAIN_Env->GetStaticMethodID(mToast_Class, "makeText", "(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;");
+        
+        if (mToast_MakeTextID == nullptr)
+            Android_Error(gMAIN_SA_Logger, gMessageStatus, "Can't locate the Toast object, Toast methods are disable\n");
+        
+    }
+
+    void JVM_Objects::SpawnToast(const char* toast_Message, Toast_Duration toast_Duration) {
+        if (mToast_MakeTextID == nullptr)
+            return;
+        
+        Android_Info(gMAIN_SA_Logger, gMessageStatus, "Spawning the Toast message: %s\n", toast_Message);
+
+        jstring tObject_Message = gMAIN_Env->NewStringUTF(toast_Message);
+        jobject toastObject = gMAIN_Env->CallStaticObjectMethod(mToast_Class, mToast_MakeTextID, mThizContext, 
+            tObject_Message, toast_Duration);
+        jmethodID toastShow = gMAIN_Env->GetMethodID(mToast_Class, "show", "()V");
+        gMAIN_Env->CallVoidMethod(toastObject, toastShow);
+        /* Deleting the reference, the JVM atomically delete own objects when the native method returns, 
+         * but at this moment, this resource is self controlled but us, this means that we need to automaticaly free
+         * the memory for avoid run out.
+        */
+        gMAIN_Env->DeleteLocalRef(tObject_Message);
+    }
+
+}
---
+++ Virtual_ENV.h
@@ -0,0 +1,22 @@
+#ifndef VIRTUAL_ENV_H
+#define VIRTUAL_ENV_H
+
+#include <jni.h>
+
+namespace OpenSA {
+    /* Toast message duration */
+    enum Toast_Duration { TOAST_SHORT = 0, TOAST_LONG = 1 };
+
+    class JVM_Objects {
+        public:
+        jobject mThizContext = nullptr;
+        jclass mToast_Class = nullptr;
+        jmethodID mToast_MakeTextID = nullptr; 
+
+        void Init_Load_Objects(int* env_Status, jobject javaContext);
+        void SpawnToast(const char* toast_Message, Toast_Duration toast_Duration);
+    };
+    
+}
+
+#endif
